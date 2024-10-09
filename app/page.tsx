7ee10b6784449099675a6b1a7aa5e3f9e00cad5c
tkmrmr <dach1024@yahoo.co.jp>
2024-10-09 10:49:29 +0900

    トプ画追加

--- app/page.tsx
+++ app/page.tsx
@@ -1,38 +1,86 @@
 "use client";
 
 import {
+  Box,
   Button,
   Flex,
   Heading,
   Input,
   useColorMode,
   useColorModeValue,
+  Text,
+  Container,
+  Center,
+  Stack,
 } from "@chakra-ui/react";
 
 export default function Home() {
   const { toggleColorMode } = useColorMode();
   const formBackground = useColorModeValue("gray.100", "gray.700");
+
   return (
-    <Flex height="400vh" alignItems="center" justifyContent="center">
-      <Flex
-        direction="column"
-        background={formBackground}
-        padding={12}
-        rounded={6}
+    <Box>
+      {/* トップ */}
+      <Box
+        bgImage="/top.png"
+        bgSize="cover"
+        bgPosition="center 0px"
+        bgRepeat="no-repeat"
+        color="black"
+        height="800px"
+        width="100%"
+        position="relative"
+        display="flex"
+        alignItems="center"
+        justifyContent="center"
+        _before={{
+          content: '""',
+          position: "absolute",
+          top: 0,
+          left: 0,
+          width: "100%",
+          height: "100%",
+          bg: "rgba(0, 0, 0, 0.3)" /* 半透明の黒をオーバーレイ */,
+          zIndex: 1,
+        }}
+        filter="brightness(90%)"
       >
-        <Heading mb={6}>Log in</Heading>
-        <Input
-          placeholder="sample@sample.com"
-          variant="filled"
-          mb={3}
-          type="email"
-        />
-        <Input placeholder="********" variant="filled" mb={6} type="password" />
-        <Button mb={6} colorScheme="teal">
-          Log in
-        </Button>
-        <Button onClick={toggleColorMode}>Toggle Color Mode</Button>
+        <Box position="relative" zIndex={2} textAlign={"center"}>
+          <Text fontSize="8xl" fontWeight="bold" color="white">
+            D@NPEN
+          </Text>
+          <Text fontSize="2xl" fontWeight="bold" color="white">
+            電気通信大学アイドルマスター研究会
+          </Text>
+        </Box>
+      </Box>
+
+      <Flex height="100vh" alignItems="center" justifyContent="center">
+        <Flex
+          direction="column"
+          background={formBackground}
+          padding={12}
+          rounded={6}
+        >
+          <Heading mb={6}>Log in</Heading>
+          <Input
+            placeholder="sample@sample.com"
+            variant="filled"
+            mb={3}
+            type="email"
+          />
+          <Input
+            placeholder="********"
+            variant="filled"
+            mb={6}
+            type="password"
+          />
+          <Button mb={6} colorScheme="teal">
+            Log in
+          </Button>
+          <Button onClick={toggleColorMode}>Toggle Color Mode</Button>
+        </Flex>
       </Flex>
-    </Flex>
+    </Box>
   );
 }

e992333669c7215542830341a4809dffc06ff90c
tkmrmr <dach1024@yahoo.co.jp>
2024-10-09 16:49:12 +0900

    フォント変更

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,10 @@
 // app/layout.tsx
 import { Providers } from "./providers";
+import { Noto_Sans_JP } from "next/font/google";
 import Header from "../components/header";
 import Footer from "../components/footer";
+
+const notoSansJP = Noto_Sans_JP({ subsets: ["latin"], weight: ["400"] });
 
 export default function RootLayout({
   children,
@@ -10,7 +13,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="ja">
-      <body>
+      <body className={notoSansJP.className}>
         <Providers>
           <Header />
           {children}

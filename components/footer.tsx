11301d2524fd94d70fafcf826a00dac319e5bcbb
tkmrmr <dach1024@yahoo.co.jp>
2024-10-10 11:18:37 +0900

    高さ調整とか

--- components/footer.tsx
+++ components/footer.tsx
@@ -2,9 +2,16 @@ import { Box, Center, Text } from "@chakra-ui/react";
 
 export default function Footer() {
   return (
-    <Box as="footer" w="100%" h="100px" borderTop={"1px solid #eee"}>
+    <Box
+      as="footer"
+      w="100%"
+      h={{ base: "60px", md: "100px" }}
+      // borderTop={"1px solid #eee"}
+    >
       <Center>
-        <Text lineHeight="100px">©️ 電気通信大学アイドルマスター研究会</Text>
+        <Text lineHeight={{ base: "60px", md: "100px" }}>
+          &copy; 電気通信大学アイドルマスター研究会
+        </Text>
       </Center>
     </Box>
   );

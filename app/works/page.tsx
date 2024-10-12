14d1093f3a731852386c87a3fd8d009ef11b4906
tkmrmr <dach1024@yahoo.co.jp>
2024-10-12 23:30:56 +0900

    準備中にした

---
+++ app/works/page.tsx
@@ -0,0 +1,10 @@
+import { Box } from "@chakra-ui/react";
+import UnderConstruction from "../../components/under-construction";
+
+export default function Works() {
+  return (
+    <Box>
+      <UnderConstruction />
+    </Box>
+  );
+}

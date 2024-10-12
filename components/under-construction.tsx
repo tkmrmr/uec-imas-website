015fefc1b05c02cd553b006a8cccdc11e4d6a074
tkmrmr <dach1024@yahoo.co.jp>
2024-10-12 23:25:00 +0900

    準備中ページ作成

---
+++ components/under-construction.tsx
@@ -0,0 +1,24 @@
+import NextLink from "next/link";
+import { Box, Heading, Center, Link, Button, Flex } from "@chakra-ui/react";
+
+export default function UnderConstruction() {
+  return (
+    <Box>
+      <Center height="50vh">
+        <Flex direction="column" align="center" gap="7">
+          <Heading>このページは準備中です</Heading>
+          <Link
+            as={NextLink}
+            href="/"
+            fontWeight="600"
+            fontSize={{ md: "13px", lg: "15px", xl: "17px" }}
+          >
+            <Button bgColor="teal.400" _hover={{ bgColor: "teal.500" }}>
+              トップページに戻る
+            </Button>
+          </Link>
+        </Flex>
+      </Center>
+    </Box>
+  );
+}

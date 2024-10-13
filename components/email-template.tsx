fff6d54aee1a152b96e3d60ded3c3694b2961e6c
tkmrmr <dach1024@yahoo.co.jp>
2024-10-14 00:45:25 +0900

    デザイン調整

--- components/email-template.tsx
+++ components/email-template.tsx
@@ -8,16 +8,60 @@ export const EmailTemplate = ({
   content: string;
 }) => {
   return (
-    <div>
-      <p>{name} さんよりお問い合わせがありました。</p>
-      <dl>
-        <dt>【お名前】</dt>
-        <dd>{name}</dd>
-        <dt>【メールアドレス】</dt>
-        <dd>{email}</dd>
-        <dt>【お問い合わせ内容】</dt>
-        <dd>{content}</dd>
-      </dl>
+    <div
+      style={{
+        fontFamily: "Arial, sans-serif",
+        backgroundColor: "#f9f9f9",
+        color: "#333",
+        padding: "20px",
+        borderRadius: "10px",
+        margin: "0 auto",
+      }}
+    >
+      <div>
+        <p style={{ marginBottom: "20px", fontSize: "18px" }}>
+          {name} さんよりD@NPENへのお問い合わせがありました。
+        </p>
+
+        <div style={{ display: "flex", flexDirection: "column" }}>
+          <div>
+            <p
+              style={{
+                fontWeight: "bold",
+                borderBottom: "1px solid #ccc",
+                fontSize: "18px",
+              }}
+            >
+              名前
+            </p>
+            <p style={{ marginTop: "10px" }}>{name}</p>
+          </div>
+          <div>
+            <p
+              style={{
+                fontWeight: "bold",
+                borderBottom: "1px solid #ccc",
+                fontSize: "18px",
+              }}
+            >
+              メールアドレス
+            </p>
+            <p style={{ marginTop: "10px" }}>{email}</p>
+          </div>
+          <div>
+            <p
+              style={{
+                fontWeight: "bold",
+                borderBottom: "1px solid #ccc",
+                fontSize: "18px",
+              }}
+            >
+              お問い合わせ内容
+            </p>
+            <p style={{ marginTop: "10px" }}>{content}</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
